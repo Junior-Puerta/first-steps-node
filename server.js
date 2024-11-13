2eeb13a5d227761458f20614b0ec55fc95af7e2b
Junior-Puerta <junior_masson@hotmail.com>
2024-11-13 18:33:05 -0300

    1ªs rotas get no fastify

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-import { createServer } from "node:http";
+// import { createServer } from "node:http";
 
-const server = createServer((req,res) => {
-  //precisa passar uma função como parâmetro
-  console.log('OLAAA')
-  res.write('me mama')
+// const server = createServer((req,res) => {
+//   //precisa passar uma função como parâmetro
+//   console.log('OLAAA')
+//   res.write('me mama')
 
-   return res.end();
+//    return res.end();
+// });
+
+// // após a criação do servidor, o server devolve o método listen
+// const PORT = 3333
+// server.listen(PORT)
+
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.listen({
+  port: 3333,
 });
 
-// após a criação do servidor, o server devolve o método listen
-const PORT = 3333
-server.listen(PORT)
+server.get("/", () => {
+  return " hello root!";
+});
 
+server.get("/node", () => {
+  return " hello Node.js!";
+});
 
